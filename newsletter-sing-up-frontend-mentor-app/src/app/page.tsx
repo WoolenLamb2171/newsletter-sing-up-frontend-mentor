618826887157e01cd449103636d7802e93ece549
WoolenLamb2171 <romanmurtukov@gmail.com>
2023-08-18 02:43:18 +0300

    add TW config

---
+++ newsletter-sing-up-frontend-mentor-app/src/app/page.tsx
@@ -0,0 +1,13 @@
+
+
+export default function Home() {
+  return (
+    <main className="">
+        <h1 className="text-Tomato">Tomato</h1>
+        <h1 className="text-Dark-Slate-Grey">Dark Slate Grey</h1>
+        <h1 className="text-Charcoal-Grey">Charcoal Grey</h1>
+        <h1 className="text-Grey">Grey</h1>
+        <h1 className="text-White">White</h1>
+    </main>
+  )
+}
---
+++ newsletter-sing-up-frontend-mentor-app/tailwind.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'Tomato': 'hsl(4, 100%, 67%)',
+        'Dark-Slate-Grey': 'hsl(234, 29%, 20%)',
+        'Charcoal-Grey': 'hsl(235, 18%, 26%)',
+        'Grey': 'hsl(231, 7%, 60%)',
+        'White': 'hsl(0, 0%, 100%)'
+      }
+    },
+  },
+  plugins: [],
+}
+export default config
