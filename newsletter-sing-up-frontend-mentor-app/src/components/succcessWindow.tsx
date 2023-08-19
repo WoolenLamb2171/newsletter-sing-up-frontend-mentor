9d4c273ec9e8d8f1ba3ca121ea71fddf6c954e63
WoolenLamb2171 <romanmurtukov@gmail.com>
2023-08-19 17:52:55 +0300

    add layout for mobile success window

--- newsletter-sing-up-frontend-mentor-app/src/components/succcessWindow.tsx
+++ newsletter-sing-up-frontend-mentor-app/src/components/succcessWindow.tsx
@@ -15,20 +15,20 @@ const SuccessWindow = () => {
     }
 
     return ( 
-        <>
-            <Image src={tick} alt="a picture of tick" priority width={55}/>
+        <div className="mx-5">
+            <Image className="mt-[35%] mb-7" src={tick} alt="a picture of tick" priority width={60}/>
 
-            <h1>Thanks for Subscribing!</h1>
+            <h1 className="mb-7 font-Bold text-4xl text-left">Thanks for Subscribing!</h1>
 
-            <p>
-                A confirmation email has been sent to <strong>{email.adress?.toString()}</strong>. 
+            <p className="font-Regular text-base text-left mb-[95%]">
+                A confirmation email has been sent to <strong className="font-Bold">{email.adress?.toString()}</strong>. 
                 Please open it and click the button inside to confirm your subscrition
             </p>
 
-            <button onClick={handleDismissClick}>
+            <button className="border rounded-lg w-full h-13 p-3 font-Regular text-White bg-Dark-Slate-Grey" onClick={handleDismissClick}>
                 Dismiss message
             </button>
-        </> 
+        </div> 
     );
 }
  
